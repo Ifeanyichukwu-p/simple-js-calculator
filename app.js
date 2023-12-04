c12d2902bca170b8259558ef1df9185e19c4ef69
Peter Ifeanyichukwu Oodo <pifeany2010@gmail.com>
2023-12-04 17:55:42 +0100

    I completed the remaining part of the JavaScript code

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const numberBtn = document.querySelectorAll('.number');
 const operetorBtn = document.querySelectorAll('.operation');
 const equalBtn = document.querySelector('.equal');
 const clearAll = document.querySelector('.clear-all');
-const removeLastNumber = document.querySelector('.del');
+const clearLastNum = document.querySelector('.del');
 
 let display1Num = '';
 let display2Num = '';
@@ -36,6 +36,7 @@ operetorBtn.forEach(operetor => {
             result = parseFloat(display2Num);
         }
         clearNums(operetorName);
+        lastOperation = operetorName;
         console.log(result);
 
     })
@@ -43,5 +44,53 @@ operetorBtn.forEach(operetor => {
 
 function clearNums(name = ''){
     display1Num += display2Num + ' ' + name + ' ';
-    display1.innerText = display1Num;
+    display1.innerText = display1Num; 
+    display2.innerText = '';
+    display2Num = '';
+    tempResult.innerText = result;
+
 }
+
+function mathOperation(){
+  if(lastOperation === 'x'){
+    result = parseFloat(result) * parseFloat(display2Num);
+  }else if(lastOperation === '+'){
+    result = parseFloat(result) + parseFloat(display2Num);
+
+  }else if(lastOperation === '-'){
+    result = parseFloat(result) - parseFloat(display2Num);
+
+  }else if(lastOperation === '/'){
+    result = parseFloat(result) / parseFloat(display2Num);
+
+  }else if(lastOperation === '%'){
+    result = parseFloat(display2Num/100);
+
+  }
+}
+
+equalBtn.addEventListener('click', (e) => {
+  if(!display1Num || !display2Num)return; 
+  dotPresent = false;
+  mathOperation();
+  clearNums();
+  display2.innerText = parseFloat(result);
+  tempResult.innerText ='';
+  display2Num = parseFloat(result);
+  display1Num = '';
+})   
+
+clearAll.addEventListener('click', (e) => {
+  display1.innerText = '';
+  display2.innerText = '0';
+  display1Num = '';
+  display2Num = '';
+  result = '';
+  tempResult.innerText = '';
+
+})
+
+clearLastNum.addEventListener('click', (e) => {
+  display2.innerText = '';
+  display2Num = '';
+});
